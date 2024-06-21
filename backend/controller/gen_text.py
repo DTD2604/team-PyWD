210577c9500afdb0a2ef9415b10a8b3d9067d306
Dowin <100296543+DTD2604@users.noreply.github.com>
2024-06-21 10:22:50 +0700

    upload coze

--- backend/controller/gen_text.py
+++ backend/controller/gen_text.py
@@ -1,11 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+import requests
+from backend.service.gen_text_service import send_message
 
-gen_text_router = APIRouter(
-    prefix="/genText",
-    tags=["genText"]
-)
+gen_text_route = APIRouter()
 
 
-@gen_text_router.get('/')
-async def gen_text():
-    return {"message": "Hello gent text"}
+# Mô hình dữ liệu yêu cầu
+class QueryRequest(BaseModel):
+    conversation_id: str
+    user: str
+    query: str
+
+
+@gen_text_route.post("/chat")
+def chat(request: QueryRequest):
+    try:
+        response = send_message(request.conversation_id, request.user, request.query)
+        return response
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=str(e))
